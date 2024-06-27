29a49c45f0c8b50f96689d4ce4084949461a6f04
Pietra <dev.nicderner@proton.me>
2024-06-27 18:23:23 -0300

    final commit

--- src/main/java/br/com/dev/tabelafipe/modelos/DadosModelos.java
+++
@@ -1,9 +0,0 @@
-package br.com.dev.tabelafipe.modelos;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosModelos(@JsonAlias("codigo") String codigo,
-                           @JsonAlias("nome") String marca) {
-}
